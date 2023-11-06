4ee8d1d181a1c57595f6d1fd7f12f1de2008c1cd
Abhishek Kadam <abhikads444@gmail.com>
2023-11-06 10:39:45 +0530

    made changes in utils

--- project_app/utils.py
+++ project_app/utils.py
@@ -30,7 +30,8 @@ class MarketValuePredict():
         self.load_model()
         
         input_data = [self.club, self.position, self.page_views, self.fpl_value, self.fpl_sel, self.fpl_points, self.region, self.new_foreign, self.age_cat, self.big_club, self.new_signing] 
+        columns_name = ["club", "position", "page_views", "fpl_value", "fpl_sel", "fpl_points", "region", "new_foreign", "age_cat", "big_club", "new_signing"]
         
-        prediction = self.model.predict([input_data])[0]
+        prediction = self.model.predict([input_data])
         
-        return prediction
+        return prediction[0]
